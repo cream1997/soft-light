2bfd7af357c7f192c82c5a9f7164a78cff264e17
cream <837800910@qq.com>
2024-01-01 23:33:03 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/cream/light/SoftLightApplication.java
@@ -0,0 +1,13 @@
+package com.cream.light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftLightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cream/light/SoftLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cream.light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftLightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
